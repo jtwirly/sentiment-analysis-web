743ee6b3e4bd54c8ac5b5f5e014ad300b2dae974
JT <jt@Taylors-MacBook-Air-2.local>
2023-03-18 03:50:20 -0400

    Commit message: adding vibe score analysis

--- src/SentimentAnalysisForm.js
+++ src/SentimentAnalysisForm.js
@@ -6,6 +6,7 @@ const SentimentAnalysisForm = () => {
   const [comment, setComment] = useState('');
   const [sentiment, setSentiment] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [sentimentDescription, setSentimentDescription] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,6 +23,17 @@ const SentimentAnalysisForm = () => {
         }
       );
       setSentiment(response.data.documentSentiment.score.toFixed(2));
+      if (response.data.documentSentiment.score > 0.75) {
+        setSentimentDescription('Good vibes!');
+      } else if (response.data.documentSentiment.score < -0.75) {
+        setSentimentDescription('Bad vibes!');
+      } else if (response.data.documentSentiment.score > 0.25) {
+        setSentimentDescription('Positive vibes!');
+      } else if (response.data.documentSentiment.score < -0.25) {
+        setSentimentDescription('Negative vibes!');
+      } else {
+        setSentimentDescription('Neutral vibes!');
+      }
       setIsLoading(false);
     } catch (error) {
       console.error(error);
@@ -46,6 +58,7 @@ const SentimentAnalysisForm = () => {
       {sentiment !== null && (
         <p className="mt-3">
           Sentiment score: <strong>{sentiment}</strong>
+          {sentimentDescription !== '' && <span> ({sentimentDescription})</span>}
         </p>
       )}
     </Form>

3d83f0cb38f7bb71ed580e5328b971f0a57cdca8
JT <jt@Taylors-MacBook-Air-2.local>
2023-03-19 03:10:56 -0400

    Trying to commit again 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <p>Curious whether that email or your partner's text is good vibes, bad vibes, or somewhere in between? Copy it into VibeCheck, and get AI's opinion on the matter.</p>
       <SentimentAnalysisForm />
       <p>Score of the vibe/sentiment ranges between -1.0 (negative) and 1.0 (positive) and corresponds to the overall emotional leaning of the text. Sample values: Good Vibes (Clearly Positive): 0.8. Bad Vibes (Clearly Negative): -0.6. Neutral Vibes (Neutral): 0.1. Mixed Vibes (Mixed): 0.0. </p>
-      <p>Want to get some AI-powered advice on how to handle the situation? Check out our sister webapp, <a href="http://dearaibby.herokuapp.com">Dear AIbby</a>!</p>
+      <p>Want to get some AI-powered advice on how to handle the situation? Check out our sister tool, <a href="http://dearaibby.herokuapp.com">Dear AIbby</a>!</p>
     </div>
   );
 }
